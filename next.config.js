ddfc8006b92077ba7ddd3c0f354c28f2aa0e9791
gyandev9808-design <gyan.dev9808@gmail.com>
2026-08-29 10:14:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Prevent TypeScript type errors from breaking Vercel builds
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Prevent ESLint rules from failing Vercel production deployments
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  // Fixes node runtime issues in client bundles
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
   },
 };
 
